baf03c3414de4d25e941c77eac033872ee5e65ef
andrew <andrewvanbelleghem@gmail.com>
2017-01-20 11:22:44 -0500

    First Commit

--- src/Launcher.java
+++ src/Launcher.java
@@ -7,9 +7,44 @@ import com.google.gson.Gson;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
 
 public class Launcher {
     public static void main(String args[]) {
+        Gson gson = new Gson();
+        BufferedReader br = null;
 
+        try {
+            br = new BufferedReader(new FileReader("./data/json/zulrah.json"));
+            Result result = gson.fromJson(br, Result.class);
+
+            if(result != null) {
+                int rand;
+//                for (Item item : result.getItems()) {
+//                    System.out.println("===================");
+//                    System.out.println(item.getName());
+//                    System.out.println("Quantity: " + item.getQuantity());
+//                    System.out.println("1/" + item.getChance() + " chance.");
+//                }
+                /*
+                 * Get chance, do formula to get drop chance percent
+                  *
+                 */
+                rand = (int) Math.random() * 100;
+                System.out.println(result.getItems().get(1).getName());
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if(br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
